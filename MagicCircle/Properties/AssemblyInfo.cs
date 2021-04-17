7d48919fe71c683460bfb72da4748ddf4d47f451
g2 <g2simm+bow@gmail.com>
2021-04-17 15:53:44 +0900

    Release magic circle

--- MagicCircle/Properties/AssemblyInfo.cs
+++ MagicCircle/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MagicCircle")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©G2  2014-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
